3b0115b88dedadee0e731d68dbe63e7d867a9f8a
Alex Holden <63453554+alexanderholden@users.noreply.github.com>
2026-05-11 03:07:20 +0100

    Update tt_um_sap_alexanderholden.v

--- src/tt_um_sap_alexanderholden.v
+++ src/tt_um_sap_alexanderholden.v
@@ -1,13 +1,12 @@
 module tt_um_sap_alexanderholden(
     input wire clk,
-    input wire clr,
+    input wire rst_n,
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
     output wire [7:0] uio_out,  // IOs: Bidirectional Output path
     output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire       ena     // will go high when the design is enabled
 );
 
     wire Cp;
@@ -44,7 +43,7 @@ module tt_um_sap_alexanderholden(
 
     controller CTRL(
         .clk(clk),
-        .clr(clr),
+        .rst_n(rst_n),
         .inst_in(ir_opcode_out),
         .Cp(Cp),
         .Ep(Ep),
@@ -105,7 +104,7 @@ module tt_um_sap_alexanderholden(
     instruction_register IR(
         .LI(Li),
         .CLK(clk),
-        .CLR(clr),
+        .rst_n(rst_n),
         .EI(Ei),
 
         .ir_bus_in(bus),
@@ -136,7 +135,7 @@ module tt_um_sap_alexanderholden(
     programcounter PC(
         .C_P(Cp),
         .nCLK(clk),
-        .nCLR(clr),
+        .nrst_n(rst_n),
 
         .pc_bus_out(pc_bus_out)
     );
