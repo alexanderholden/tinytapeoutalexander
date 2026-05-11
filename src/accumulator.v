bb2279148d2ae2079ca955a319a131b9978f5170
Alex Holden <63453554+alexanderholden@users.noreply.github.com>
2026-05-11 18:00:38 +0000

    hehehe MOFIDIEF

--- src/accumulator.v
+++ src/accumulator.v
@@ -4,21 +4,22 @@ module accumulator( // WORKING
     input  wire       EA,          // active high enable output to bus
     input  wire [7:0] acc_bus_in,  // input bus
     output wire [7:0] acc_bus_out, // output bus
-    output wire [7:0] add_sub_input // output for ALU
+    output wire [7:0] add_sub_input, // output for ALU
+    input wire rst_n
 );
 
     reg [7:0] accumulator_reg = 8'b0;
 
     // Load process
-    always @(posedge CLK) begin
-        if (LA)
+    always @(posedge CLK or negedge rst_n) begin
+        if(!rst_n)
+            accumulator_reg <= 8'b0;
+        else if (LA)
             accumulator_reg <= acc_bus_in;
     end
 
-    // Output onto bus (tri-state behavior)
     assign acc_bus_out = accumulator_reg;
 
-    // Always drive add/sub input
     assign add_sub_input = accumulator_reg;
 
 endmodule
--- src/reg_b.v
+++ src/reg_b.v
@@ -2,14 +2,17 @@ module b_register(
     input  wire       LB,            // load enable
     input  wire       CLK,           // clock
     input  wire [7:0] b_bus_in,      // input bus
-    output wire [7:0] b_add_sum_in   // output to add/sub module
+    output wire [7:0] b_add_sum_in,   // output to add/sub module
+    input wire rst_n
 );
 
     reg [7:0] b_reg = 8'b0;
 
     // Load process
-    always @(posedge CLK) begin
-        if (LB)
+    always @(posedge CLK or negedge rst_n) begin
+        if (!rst_n)
+            b_reg <= 8'b0;
+        else if (LB)
             b_reg <= b_bus_in;
     end
 
