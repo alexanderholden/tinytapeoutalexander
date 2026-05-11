980ff80e6719401fef4ea722cecee2767c681c43
Alex Holden <63453554+alexanderholden@users.noreply.github.com>
2026-05-11 03:00:40 +0100

    Update tt_um_sap_alexanderholden.v

--- src/tt_um_sap_alexanderholden.v
+++ src/tt_um_sap_alexanderholden.v
@@ -1,6 +1,13 @@
-module tt_um_sap_alexanderholden(
+module tt_um_sap_alexanderholden_1(
     input wire clk,
-    input wire clr
+    input wire clr,
+    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       rst_n     // reset_n - low to reset
 );
 
     wire Cp;
